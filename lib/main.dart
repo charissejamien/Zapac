46fabf551719ba14bce179b44f5b64e960711907
Charisse Jamien <jamiencharisse@gmail.com>
2025-06-25 19:51:16 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'auth_screen.dart';
 
 void main() {
@@ -7,3 +8,5 @@ void main() {
     debugShowCheckedModeBanner: false,
   ));
 }
+
+
